c46125aacaf99b58210212d441ab3466d6ead4da
Georgi Berchev <g.berchev@gmail.com>
2019-04-22 15:23:18 +0300

    add app file

--- app.rb
+++ app.rb
@@ -1,3 +1,18 @@
-#!/usr/bin/env rubyI
+#!/usr/bin/env ruby
 
-p 1
+content = File.open("output.txt", "r") { |file| file.read }
+puts content
+
+number = content.to_i
+
+if number == 4
+  File.open("output.txt", "w") do |file|
+    number = 1
+    file.write "#{number}" 
+  end
+else
+  File.open("output.txt", "w") do |file|
+    number = number + 1
+    file.write "#{number}"
+  end
+end
