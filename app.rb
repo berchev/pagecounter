c343cbc7cea275652fccc08c58d33e077e98abc6
Georgi Berchev <g.berchev@gmail.com>
2019-04-23 10:21:23 +0300

    update app.rb to be infinity counter

--- app.rb
+++ app.rb
@@ -4,15 +4,7 @@ content = File.open("output.txt", "r") { |file| file.read }
 puts content
 
 number = content.to_i
-
-if number == 4
-  File.open("output.txt", "w") do |file|
-    number = 1
-    file.write "#{number}" 
-  end
-else
-  File.open("output.txt", "w") do |file|
-    number = number + 1
-    file.write "#{number}"
-  end
+File.open("output.txt", "w") do |file|
+  number = number + 1
+  file.write "#{number}"
 end
